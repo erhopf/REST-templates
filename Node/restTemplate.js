b5e261eaf7ab0156b2cbd4d2fdc1e3cec8fd5b5c
erhopf <erhopf@microsoft.com>
2018-10-26 11:18:58 -0700

    After reviewing the updated code, while it used best practices, such as writing reusable code as functions, it required more effort to read, and wasn't necessarily scalable. Will revisit best practices for Tutorials and other samples.

--- Node/restTemplate.js
+++ Node/restTemplate.js
@@ -21,37 +21,26 @@ The sample headers are pretty straight forward. This sample assumes that the
 subscription key can be passed as an Ocp-Apim-Subscription-Key header, however,
 there are APIs that will require a Authorization header and a Bearer token.
 Only one is required. */
-function httpRequest(input){
-    const options = {
-        method: 'POST',
-        baseUrl: 'https://api.cognitive.microsofttranslator.com/',
-        url: 'translate',
-        qs: {
-          'api-version': '3.0',
-          'to': 'de'
-        },
-        headers: {
-          'Ocp-Apim-Subscription-Key': subscriptionKey,
-          'Content-type': 'application/json',
-        },
-        body: [{
-              'text': input
-        }],
-        json: true,
-    };
-
-    /* The result is stringified to ensure that all objects are printed correctly
-    to the terminal. */
-    request(options, function(err, res, body){
-        console.log(JSON.stringify(body, null, 4));
-    });
+const options = {
+    method: 'POST',
+    baseUrl: 'https://api.cognitive.microsofttranslator.com/',
+    url: 'translate',
+    qs: {
+      'api-version': '3.0',
+      'to': 'de'
+    },
+    headers: {
+      'Ocp-Apim-Subscription-Key': subscriptionKey,
+      'Content-type': 'application/json',
+    },
+    body: [{
+          'text': 'Hello World! Welcome to Azure!'
+    }],
+    json: true,
 };
 
-/* The first argument is the path to node, the second is our filename, and the
-third is your query. If the query is absent, a default query of
-"Hello world!" is used. Feel free to adjust this for your use case. */
-const input = process.argv[2] || 'Hello World!'
-
-/* Calls the function that we created to house the HTTP request. The value
-provided for input will be passed to the API in the JSON body. */
-httpRequest(input);
+/* The result is stringified to ensure that all objects are printed correctly
+to the terminal. */
+request(options, function(err, res, body){
+    console.log(JSON.stringify(body, null, 4));
+});

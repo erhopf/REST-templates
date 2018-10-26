06eb21866cc06fee6952b08a4d7aa383c14bcaa1
erhopf <erhopf@microsoft.com>
2018-10-26 11:10:02 -0700

    Moving the HTTP call into a function. Setting a default value for input, for use cases where an argument is not passed to the function.

--- Node/restTemplate.js
+++ Node/restTemplate.js
@@ -8,7 +8,7 @@ string, then comment these lines out.
 
 If you want to set your subscription key as a string, replace the value for
 the Ocp-Apim-Subscription-Key header as a string. */
-let subscriptionKey = process.env.NAME_OF_YOUR_ENV_VARIABLE;
+const subscriptionKey = process.env.YOUR_ENV_VARIABLE;
 if (!subscriptionKey) {
   throw new Error('Environment variable for your subscription key is not set.')
 };
@@ -18,29 +18,40 @@ required parameters, headers, and body. Make note that since the body
 input is JSON, that you won't need to parse the response.
 
 The sample headers are pretty straight forward. This sample assumes that the
-subscription key can be passed as anOcp-Apim-Subscription-Key header, however,
+subscription key can be passed as an Ocp-Apim-Subscription-Key header, however,
 there are APIs that will require a Authorization header and a Bearer token.
 Only one is required. */
-const options = {
-    method: 'POST',
-    baseUrl: 'https://api.cognitive.microsofttranslator.com/',
-    url: 'translate',
-    qs: {
-      'api-version': '3.0',
-      'to': 'de'
-    },
-    headers: {
-      'Ocp-Apim-Subscription-Key': subscriptionKey,
-      'Content-type': 'application/json',
-    },
-    body: [{
-          'text': 'Hello World!'
-    }],
-    json: true,
+function httpRequest(input){
+    const options = {
+        method: 'POST',
+        baseUrl: 'https://api.cognitive.microsofttranslator.com/',
+        url: 'translate',
+        qs: {
+          'api-version': '3.0',
+          'to': 'de'
+        },
+        headers: {
+          'Ocp-Apim-Subscription-Key': subscriptionKey,
+          'Content-type': 'application/json',
+        },
+        body: [{
+              'text': input
+        }],
+        json: true,
+    };
+
+    /* The result is stringified to ensure that all objects are printed correctly
+    to the terminal. */
+    request(options, function(err, res, body){
+        console.log(JSON.stringify(body, null, 4));
+    });
 };
 
-/* The result is stringified to ensure that all objects are printed correctly
-to the terminal. */
-request(options, function(err, res, body){
-    console.log(JSON.stringify(body, null, 4));
-});
+/* The first argument is the path to node, the second is our filename, and the
+third is your query. If the query is absent, a default query of
+"Hello world!" is used. Feel free to adjust this for your use case. */
+const input = process.argv[2] || 'Hello World!'
+
+/* Calls the function that we created to house the HTTP request. The value
+provided for input will be passed to the API in the JSON body. */
+httpRequest(input);

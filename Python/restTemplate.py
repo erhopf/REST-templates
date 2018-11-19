87026cae23202bf1c4ea0750db5012f48765f8da
erhopf <erhopf@microsoft.com>
2018-11-19 11:50:15 -0800

    Updating the Python sample to confine the HTTP call to a function. Additionally, checking to see if app is run as __main__.

--- Python/restTemplate.py
+++ Python/restTemplate.py
@@ -20,35 +20,40 @@ else:
 # Here you define the endpoint, the resource and any required parameters.
 # For APIs with a single route, it's not necessary to separate base_url and
 # path. The values provided are from: https://docs.microsoft.com/azure/cognitive-services/translator/reference/v3-0-translate
-base_url = 'https://api.cognitive.microsofttranslator.com'
-path = '/translate?api-version=3.0'
-params = '&to=de&to=it'
-constructed_url = base_url + path + params
+def function_name():
+    base_url = 'https://api.cognitive.microsofttranslator.com'
+    path = '/translate?api-version=3.0'
+    params = '&to=de&to=it'
+    constructed_url = base_url + path + params
 
-# The sample headers are pretty straight forward. Not all services require
-# X-ClientTraceId. If it's not used in your service it should be removed.
-# This sample assumes that the subscription key can be passed as an
-# Ocp-Apim-Subscription-Key header, however, there are APIs that will require
-# a Authorization header and a Bearer token. Only one is required.
-headers = {
-    'Ocp-Apim-Subscription-Key': subscriptionKey,
-    'Content-type': 'application/json',
-    'X-ClientTraceId': str(uuid.uuid4())
-}
+    # The sample headers are pretty straight forward. Not all services require
+    # X-ClientTraceId. If it's not used in your service it should be removed.
+    # This sample assumes that the subscription key can be passed as an
+    # Ocp-Apim-Subscription-Key header, however, there are APIs that will require
+    # a Authorization header and a Bearer token. Only one is required.
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscriptionKey,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
 
-# To ensure readability, the JSON body is hardcoded for this sample. In a
-# tutorial, you would more than likely want to leverage a class for this data
-# structure.
-body = [{
-    'text': 'Hello World!'
-}]
+    # To ensure readability, the JSON body is hardcoded for this sample. In a
+    # tutorial, you would more than likely want to leverage a class for this data
+    # structure. Please note: The Translator Text example provided below
+    # take a list of objects.
+    body = [{
+        'text': 'Hello World!'
+    }]
 
-# This is a POST request using the requests library. All REST methods are
-# available via requests. For example: requests.get(). The response is stored
-# as raw JSON.
-request = requests.post(constructed_url, headers=headers, json=body)
-response = request.json()
+    # This is a POST request using the requests library. All REST methods are
+    # available via requests. For example: requests.get(). The response is stored
+    # as raw JSON.
+    request = requests.post(constructed_url, headers=headers, json=body)
+    response = request.json()
 
-# The dumps method is used to prettify the response so that it's human readable
-# when printed to terminal/console.
-print(json.dumps(response, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
+    # The dumps method is used to prettify the response so that it's human readable
+    # when printed to terminal/console.
+    print(json.dumps(response, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
+
+if __name__ = "__main__":
+    function_name()
